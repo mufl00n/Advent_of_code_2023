f2dae5b697149c20614f38abb6c47d8747ef1561
mufl00n <lubos.lehuta@gmail.com>
2024-10-09 16:05:20 +0200

    Part 1 completed

--- Day2/Cube Conundrum.py
+++ Day2/Cube Conundrum.py
@@ -1,3 +1,22 @@
-red = 12
-green = 13
-blue = 14
+with open('sharp.txt') as f:
+    lines = f.readlines()
+    total = 0
+    for line in lines:
+        item = {'red': 0, 'green': 0, 'blue': 0}
+        game_id = int(line.strip().split(':')[0].split()[1])
+        game_set = line.replace(f'Game {game_id}:', '').strip().split(';')
+        correct = True
+        for set in game_set:
+            set = set.split(', ')
+            for part in set:
+                part = part.split()
+                item[part[1]] = int(part[0])
+            if item['red'] > 12 or item['green'] > 13 or item['blue'] > 14:
+                correct = False
+                break
+        if correct == True:
+            total += game_id
+
+print('solution is: ', total)
+
+
